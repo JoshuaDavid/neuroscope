fc45a8a618a66727952607ad577bb20c26649b13
JoshuaDavid <joshuad93@gmail.com>
2023-07-15 01:44:12 -0700

    Continue to separate class/function definitions from inline code

--- neuroscope/config.py
+++ neuroscope/config.py
@@ -4,5 +4,12 @@ import os
 dotenv.load_dotenv()
 
 WEBSITE_VERSION = 4
+
+CACHE_DIR = Path.home() / ("cache")
+REPO_ROOT = Path.home() / ("hf_repos/")
+OLD_CHECKPOINT_DIR = Path.home() / ("solu_project/solu_checkpoints/")
+CHECKPOINT_DIR = Path.home() / ("solu_project/saved_models/")
+
 MAKE_META_FILES = bool(os.getenv('MAKE_META_FILES', True))
 WEBSITE_DIR     = os.getenv('WEBSITE_DIR', '/workspace/neuroscope/v{WEBSITE_VERSION}')
+IN_IPYTHON      = False
